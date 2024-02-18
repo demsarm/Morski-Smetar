b5ae39bd75dfd3f34d2b9f386dffecdf3e2e48b7
demsarm <demsarmartin218@gmail.com>
2024-02-18 13:39:49 +0100

    Add basic config file

---
+++ Config/Config.cpp
@@ -0,0 +1,11 @@
+//
+// Created by martind on 17.2.2024.
+//
+
+#include "Config.hpp"
+
+namespace WindowData {
+	int SCREEN_WIDTH = 800;
+	int SCREEN_HEIGHT = 600;
+	int SCREEN_FPS = 60;
+}
---
+++ Config/Config.hpp
@@ -0,0 +1,14 @@
+//
+// Created by martind on 17.2.2024.
+//
+
+#ifndef MORSKI_SMETAR_CONFIG_HPP
+#define MORSKI_SMETAR_CONFIG_HPP
+
+namespace WindowData {
+	extern int SCREEN_WIDTH;
+	extern int SCREEN_HEIGHT;
+	extern int SCREEN_FPS;
+}
+
+#endif //MORSKI_SMETAR_CONFIG_HPP
